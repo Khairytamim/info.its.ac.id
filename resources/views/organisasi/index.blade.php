a29a1044889b2110eac0115885ecc07b1411e391
Muhammad Hilman <hlmn.hg@gmail.com>
2017-06-22 09:46:50 +0700

    hapus gambar contoh

--- resources/views/organisasi/index.blade.php
+++ resources/views/organisasi/index.blade.php
@@ -31,7 +31,7 @@
 				<h1>ORGANIGRAM</h1>
 			</div>
 			<div class="col-sm-12 col-md-6">
-				<img src="https://pbs.twimg.com/profile_images/378800000832652291/ab3c18c832dab4c08104f0a9b337a34b.jpeg" width="100%" height="100%">
+				{{-- <img src="https://pbs.twimg.com/profile_images/378800000832652291/ab3c18c832dab4c08104f0a9b337a34b.jpeg" width="100%" height="100%"> --}}
 			</div>
 		</div>
 	</div>
