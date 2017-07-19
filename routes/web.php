26661dd5c75e22f02dcb749ad0fd654daba8f4e5
Muhammad Hilman <hlmn.hg@gmail.com>
2017-07-19 11:02:33 +0700

    disable auth routes

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/admin', function () {
     return redirect('/admin/mailbox');
 });
 
-Auth::routes();
+// Auth::routes();
 
 Route::group(['prefix' => 'admin/mailbox'], function () {
 	Route::get('/', 'MailboxController@index')->name('mailbox');
@@ -73,9 +73,9 @@ Route::group(['prefix' => 'tanyakan'], function () {
 	Route::get('/list', 'PertanyaanController@list')->name('listtanyakan');  
 });
 
-// Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-// Route::post('login', 'Auth\LoginController@login');
-// Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
         // Registration Routes...
 // Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
