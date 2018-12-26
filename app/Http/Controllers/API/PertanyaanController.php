66aa38ba176621792aac9b7710d7e58a22de99df
Muhammad Adib Arinanda <arinanda.adib@gmail.com>
2018-12-26 21:00:15 +0700

    create informasi api done + refactor

--- app/Http/Controllers/API/PertanyaanController.php
+++ app/Http/Controllers/API/PertanyaanController.php
@@ -18,43 +18,26 @@ use DB;
 class PertanyaanController extends Controller
 {
     public function createInformasi(Request $request) {
-		// $contents = $request->json()->all();
 
         $create = new Pertanyaan;
         $create->id_pertanyaan = Uuid::generate(4);
-        $create->nama_penanya = $request->nama;
-        $create->alamat_penanya = $request->alamat;
-        $create->nohp_penanya = $request->kontak;
-        $create->email_penanya = $request->email;
-        $create->judul_pertanyaan = $request->judul;
-        $create->pertanyaan = $request->pertanyaan;
-        // $create->nama_penanya = $contents['nama'];
-        // $create->alamat_penanya = $contents['alamat'];
-        // $create->nohp_penanya = $contents['kontak'];
-        // $create->email_penanya = $contents['email'];
-        // $create->judul_pertanyaan = $contents['judul'];
-        // $create->pertanyaan = $contents['pertanyaan'];
+        $create->nama_penanya = $request['nama'];
+        $create->alamat_penanya = $request['alamat'];
+        $create->nohp_penanya = $request['kontak'];
+        $create->email_penanya = $request['email'];
+        $create->judul_pertanyaan = $request['judul'];
+        $create->pertanyaan = $request['pertanyaan'];
         $create->tipe_layanan = 'info';
 
-        // $file64 = $contents['ktp'];
-        // dd($contents['ktp']);
-        $file64 = $request->ktp;
-        $file = base64_decode($file64);
-
-        return response()->json($file, 201);
-        // dd($file);
-        // $filename = 'ktp/' . Uuid::generate(4) . '.' . $file->extension();
+        $file = $request['ktp'];
+        
         $filename = 'ktp/' . Uuid::generate(4) . '.' . $file->getClientOriginalExtension();
 		Storage::disk('public')->put($filename, File::get($file));
 		$create->ktp = "storage/$filename";
         
         $create->save();
-        // Mail::to($contents['email'])->send(new AddPertanyaan($create->id_pertanyaan));
-        // $encodean = base64_encode($request->ktp);
-        // return response()->json(basename(base64_decode($encodean)), 201);
-        // return response()->json($contents, 201);
-        // return response()->json($file, 201);
-        // return response()->setStatusCode(200);
+        Mail::to($request['email'])->send(new AddPertanyaan($create->id_pertanyaan));
+        return response()->json($request, 201);
     }
 
     public function createKeberatan(Request $request) {
