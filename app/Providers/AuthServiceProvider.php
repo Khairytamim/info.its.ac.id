c352fa6371f38492e11bb6516e732a7de6ef13ca
Muhammad Adib Arinanda <arinanda.adib@gmail.com>
2019-02-21 15:00:28 +0700

    add passport in service provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Laravel\Passport\Passport;
+use Laravel\Passport\PassportServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
